eab4af77531d7b1df7d638bde06fce79b01bd78e
Kevin Barralon <me@kbarralon.com>
2018-10-08 17:36:25 -0400

    Add a better way to find base 64 file extension

--- common/drf/fields.py
+++ common/drf/fields.py
@@ -1,7 +1,8 @@
 import base64
-import imghdr
 import uuid
+from mimetypes import guess_extension
 
+import magic
 from django.core.files.base import ContentFile
 from rest_framework import serializers
 
@@ -26,13 +27,14 @@ class Base64ImageField(serializers.ImageField):
             # Generates a unique file name and determines the file extension.
             file_name = str(uuid.uuid4())[:12]
             file_extension = self.get_file_extension(file_name, decoded_file)
-            complete_file_name = f'{file_name}.{file_extension}'
+            complete_file_name = f'{file_name}{file_extension}'
 
             data = ContentFile(decoded_file, name=complete_file_name)
 
         return super().to_internal_value(data)
 
     def get_file_extension(self, file_name, decoded_file):
-        extension = imghdr.what(file_name, decoded_file)
-        extension = 'jpg' if extension == 'jpeg' else extension
+        mime = magic.from_buffer(decoded_file, mime=True)
+        extension = guess_extension(mime)
+        extension = '.jpg' if extension == '.jpe' else extension
         return extension
